730dde7f20c1c48b78a0c2c5098e0dca0cd2748c
Zulfat <zulfat.sabirov.04@bk.ru>
2022-03-19 12:40:00 +0300

    gfdfg

--- sabirov.telbot.db/Models/DBOperations.cs
+++ sabirov.telbot.db/Models/DBOperations.cs
@@ -26,17 +26,17 @@ namespace sabirov.telbot.db.Models
                 db.SaveChanges();
             }
         }
-        public static async Task<TelUser> GetUser(string username, long chatid)
+        public static TelUser GetUser(string username, long chatid)
         {
             using (TelUserContext db = new TelUserContext())
             {
                 var user = db.telUsers.FirstOrDefault(x => x.Name == username);
                 if (user == null)
                 {
-                    await TelApi.SendMessage("Пройдите регистрацию! Вас нет в моей базе!", chatid);
+                    TelApi.SendMessage("Пройдите регистрацию! Вас нет в моей базе!", chatid);
                     return null;
                 }
-                await TelApi.SendMessage("Пользователь найден!", chatid);
+                TelApi.SendMessage("Пользователь найден!", chatid);
                 db.SaveChanges();
                 return user;
             }
